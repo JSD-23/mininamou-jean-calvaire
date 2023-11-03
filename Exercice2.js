f8408180377eea35412302507b702ad743697ced
unknown <lecalviniste@gmail.com>
2023-11-03 09:35:25 +0100

    resolution de l'exercice 2

--- Exercice2.js
+++ Exercice2.js
@@ -1,3 +1,14 @@
 function maxProfit(prix, prix_size) {
     // Votre code ici
+    let profitMax = 0;
+    let prixMinimum = prix[0];
+
+    for (let i = 1; i < prix_size; i++) {
+        profitMax = Math.max(profitMax, prix[i] - prixMinimum);
+        prixMinimum = Math.min(prixMinimum, prix[i]);
+    }
+
+    return profitMax;
 }
+
+
