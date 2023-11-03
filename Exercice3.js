203d062a6425e5f2ad15775ea46fbc232f315604
unknown <lecalviniste@gmail.com>
2023-11-03 10:00:25 +0100

    resolution de l'exercice 3

--- Exercice3.js
+++ Exercice3.js
@@ -1,18 +1,35 @@
 class LRUCache {
     constructor(capacite) {
-        // Implémenter la logique d'initialisation
+        this.capacite = capacite;
+        this.cache = new Map(); 
     }
 
     get(cle) {
-        // Implémenter la logique de récupération
-        return 0;
+        if (this.cache.has(cle)) {
+            const valeur = this.cache.get(cle);
+            this.cache.delete(cle);
+            this.cache.set(cle, valeur);
+            return valeur;
+        } else {
+            return -1; 
+        }
     }
 
     put(cle, valeur) {
-        // Implémenter la logique d'insertion
+        if (this.cache.has(cle)) {
+            this.cache.delete(cle);
+        } else if (this.cache.size >= this.capacite) {
+            const premiereCle = this.cache.keys().next().value;
+            this.cache.delete(premiereCle);
+        }
+
+        this.cache.set(cle, valeur);
     }
 
     free() {
-        // Implémenter la logique de libération
+        this.cache.clear(); 
     }
+
 }
+
+
